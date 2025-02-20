2db1b91348d9703ef3b58088ff086a881f622c75
Preston Fegley <fegley.preston@gmail.com>
2025-02-20 01:34:26 +0000

    break 1 snapshot; dice, tacos, bitcoins

--- app/controllers/tacos_controller.rb
+++ app/controllers/tacos_controller.rb
@@ -1,7 +1,9 @@
 class TacosController < ApplicationController
 
   def index
-    # render :template => "tacos/index"
+    @fillings = ["Carnitas", "Al Pastor", "Steak", "Fish", "Veggie"]
+
+    render :template => "tacos/index"  # Note: you don't need this if you're following the pattern of file use (e.g. your index file is under views>tacos)
   end
 
 end
